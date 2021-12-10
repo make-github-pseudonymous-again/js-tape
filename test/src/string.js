39f50fc9f9750c2c5edba4901ed067fefd1f1e54
make-github-pseudonymous-again <5165674+make-github-pseudonymous-again@users.noreply.github.com>
2021-12-10 16:49:25 +0100

    :recycle: refactor(test): Simplify by replacing t.deepEqual with t.is.

--- test/src/string.js
+++ test/src/string.js
@@ -9,5 +9,5 @@ import {fromString, toString} from '#module';
 test('can read from string', async (t) => {
 	const input = '\n\n\n    1 ,\t22\n, 333\t,\n-44 ,\t-5 \t\n  \t';
 	const myTape = fromString(input);
-	t.deepEqual(await toString(myTape), input);
+	t.is(await toString(myTape), input);
 });
